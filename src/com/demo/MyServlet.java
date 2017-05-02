e201b334e7da7fc9c3783127ef194de530fcb28c
xionghaibo <1489280205@qq.com>
2017-05-02 17:18:27 +0800

    第一课

---
+++ src/com/demo/MyServlet.java
@@ -0,0 +1,30 @@
+package com.demo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doGet(req, resp);
+		//logic HttpServletRequest请求,HttpServletResponse 响应
+		PrintWriter out = resp.getWriter();
+		out.print("hello servlet!");
+		out.close();
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doPost(req, resp);
+		doGet(req, resp);
+	}
+
+}
