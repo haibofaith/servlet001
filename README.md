4ccf598cae08f39d3091e9ecefcd6c4c066d8e4f
xionghaibo <1489280205@qq.com>
2017-05-03 16:53:44 +0800

    第三课

--- README.md
+++ README.md
@@ -12,3 +12,41 @@ java.lang.ClassNotFoundException: com.demo
     <servlet-class>com.demo</servlet-class>
 </servlet>
 我的路径为com.demo.MyServlet，而我只是写了包名。
+
+2.
+重启之后tomcat不能启动
+Server Tomcat v8.0 Server at localhost failed to start.
+
+解决：去掉注解@WebServlet("/LoginServlet")
+
+3.servlet生命周期
+构造方法-init初始化-doget／dopost-destory
+
+4.servlet输出一个html页面(jsp)
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.print("<h1>hello servlet</h1>");
+}
+
+5.获得servlet初始化参数
+web.xml:
+<context-param>
+  <param-name>servlet_name</param-name>
+  <param-value>servlet_value</param-value>
+</context-param>
+MyServlet:
+String str = this.getServletContext().getInitParameter("servlet_name");
+		System.out.println(str);
+
+6.页面导航
+1、请求重定向
+resp.sendRedirect("http://www.baidu.com");
+2、foward只跳转
+request.getRequestDispatcher("hello.html").forward(request, response);
+3、include请求包含
+request.getRequestDispatcher("hello.html").include(request, response);		
+
+7.获取参数
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println("username:"+username+" password:"+password);
---
+++ src/com/demo/RegisterServlet.java
@@ -0,0 +1,42 @@
+package com.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		System.out.println("username:"+username+" password:"+password);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
